22bc92e9502a152e61f716dc0c1ebb5a3dec2072
= <=>
2015-09-16 11:18:01 -0700

    fix pluraliztion

--- controllers/users.js
+++ controllers/users.js
@@ -1,49 +1,56 @@
 var db = require('../models/index');
 
 // INDEX
-app.get('/user', function(req, res){
+app.get('/users', function(req, res){
   db.User.find({}, function(err, users){
     res.render('users/index', {users: users});
   });
 });
 
 // NEW
-app.get('/user/new', function(req, res){
+app.get('/users/new', function(req, res){
   res.render('users/new');
 });
 
 
 // CREATE
-app.post('/user', function(req, res){
+app.post('/users', function(req,res){
   var newUser = req.body.user;
-  db.User.create({ newUser, function(err, newUser){
-      if (err) {
+  db.User.create(newUser,
+  function(err, newUser){
+    if (err){
       console.log(err);
       res.render('users/new');
     } else {
       console.log(newUser);
       res.redirect('/users');
     }
-    }
   });
 });
 
+
 // SHOW
-app.get('/user/:id', function(req, res){
-  
+app.get('/users/:id', function(req, res){
+  db.User.findById(req.params.id)
+    .populate('post')
+    .exec(function(err, user){
+      res.render('users/show', {user: user});
+    });
 });
 
 // EDIT
-app.get('/user/:id/edit', function(req, res){
-
+app.get('/users/:id/edit', function(req, res){
+  db.User.findById(req.params.id, function(err, user){
+    res.render('user/edit', {user: user});
+  });
 });
 
 // UPDATE
-app.put('/user/:id', function(req, res){
+app.put('/users/:id', function(req, res){
 
 });
 
 // DESTROY
-app.delete('/user/:id', function(req, res){
+app.delete('/users/:id', function(req, res){
 
 });

8ce97a36755cd6e689a6e257851774c993d11dce
= <=>
2015-09-16 13:56:08 -0700

    fix posts show

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,23 +2,42 @@ var db = require('../models/index');
 
 // INDEX
 app.get('/posts', function(req, res){
-
+  db.Post.find({}, function(req, res){
+    res.render('posts/index');
+  });
 });
 
 // NEW
 app.get('/posts/new', function(req, res){
-
+  res.render('posts/new');
 });
 
 
 // CREATE
 app.post('/posts', function(req, res){
-
+  var newPost = req.body;
+  db.Post.create(newPost, function(err, post){
+    if(err){
+      console.log(err);
+      res.render('posts/new', {post: post});
+    } else {
+      res.redirect('/posts');
+    }
+  });
 });
 
 // SHOW
 app.get('/posts/:id', function(req, res){
-  
+  db.Post.findById(req.params.id)
+    .populate('user')
+    .exec(function(err, post){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(post);
+        res.render('posts/show', {post: post});
+      }
+    });
 });
 
 // EDIT

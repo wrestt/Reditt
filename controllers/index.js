ee0428d50bc576c3337f5c7862ba6333555f6124
Teagan Wrest <wrestteagan@gmail.com>
2015-09-17 10:32:56 -0700

    added content

--- controllers/index.js
+++ controllers/index.js
@@ -8,14 +8,11 @@ require('./comments');
 require('./signups');
 require('./logins');
 
-<<<<<<< HEAD
-// app.get()
-=======
+
 app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
->>>>>>> aeef20a338d91a387f4d7216ad0fd361c6d89289
 
 app.get('*', function(req,res){
   res.render('404');
